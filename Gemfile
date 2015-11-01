24500003118a647994f14046fe0d12e04a3fc8f7
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2015-11-01 10:31:42 +0100

    [FIX] Fixed ci build

--- Gemfile
+++ Gemfile
@@ -1,2 +1,2 @@
 source 'https://rubygems.org'
-gem 'albacore', '~> 2.5.4'
+gem 'albacore', '~> 2.3.22'
--- src/Jajo.Cms.FubuMVC1/Properties/AssemblyInfo.cs
+++ src/Jajo.Cms.FubuMVC1/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("969d4172-45a1-4698-8cc8-d07cfcf04887")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
