11cfc0a786cebd12c67ded9fb5bac1de2affba37
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2015-11-01 12:12:14 +0100

    Added context extensions

---
+++ src/Jajo.Cms/CmsContextExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Jajo.Cms
+{
+    public static class CmsContextExtensions
+    {
+        public static TResult GetDataFromContext<TContext, TResult>(this ICmsContext cmsContext, Func<TContext, TResult> getResult) where TContext : class, IRequestContext
+        {
+            var context = cmsContext.LastContextOf<TContext>();
+
+            return context == null ? default(TResult) : getResult(context);
+        }
+    }
+}
