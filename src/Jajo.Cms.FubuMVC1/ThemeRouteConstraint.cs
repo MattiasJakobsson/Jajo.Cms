585ee009276ed7b1ed830c891039e599ccbd650f
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2016-01-09 19:23:35 +0100

    [FEATURE] Added support for constrains

---
+++ src/Jajo.Cms.FubuMVC1/ThemeRouteConstraint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using Jajo.Cms.Theme;
+using StructureMap;
+
+namespace Jajo.Cms.FubuMVC1
+{
+    public class ThemeRouteConstraint : IRouteConstraint
+    {
+        private readonly Type _requiredActiveTheme;
+        private readonly IContainer _container;
+
+        public ThemeRouteConstraint(Type requiredActiveTheme, IContainer container)
+        {
+            _requiredActiveTheme = requiredActiveTheme;
+            _container = container;
+        }
+
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            var themes = _container.GetAllInstances<ITheme>();
+
+            return themes.Any(x => x.GetType() == _requiredActiveTheme && x.IsActive());
+        }
+    }
+}
---
+++ src/Jajo.Cms/Theme/ThemeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Jajo.Cms.Theme
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class ThemeAttribute : Attribute
+    {
+        public ThemeAttribute(Type theme)
+        {
+            Theme = theme;
+        }
+
+        public Type Theme { get; private set; }
+    }
+}
