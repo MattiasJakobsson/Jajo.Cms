41436abadfa6f439d5c44d3a1bf62c3b7997d59f
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2016-02-12 12:40:37 +0100

    [FIX]

--- src/Jajo.Cms.FubuMVC1/CmsFubuPageExtensions.cs
+++ src/Jajo.Cms.FubuMVC1/CmsFubuPageExtensions.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web;
 using FubuMVC.Core.View;
 using Jajo.Cms.Components;
 using Jajo.Cms.Rendering;
 using Jajo.Cms.Templates;
 using Jajo.Cms.Theme;
+using NLog;
 
 namespace Jajo.Cms.FubuMVC1
 {
@@ -36,14 +38,25 @@ namespace Jajo.Cms.FubuMVC1
 
         public static IHtmlString ParseText(this IFubuPage page, string text, ParseTextOptions options = null, ITheme theme = null)
         {
-            var cmsRenderer = page.ServiceLocator.GetInstance<ICmsRenderer>();
-            var cmsContext = page.ServiceLocator.GetInstance<ICmsContext>();
+            try
+            {
+                var cmsRenderer = page.ServiceLocator.GetInstance<ICmsRenderer>();
+                var cmsContext = page.ServiceLocator.GetInstance<ICmsContext>();
 
-            theme = theme ?? cmsContext.GetCurrentTheme();
+                theme = theme ?? cmsContext.GetCurrentTheme();
 
-            var result = cmsRenderer.ParseText(text, cmsContext, theme, options);
+                var result = cmsRenderer.ParseText(text, cmsContext, theme, options);
 
-            return new HtmlString(result.Read());
+                return new HtmlString(result.Read());
+            }
+            catch (Exception ex)
+            {
+                var logger = page.ServiceLocator.GetInstance<Logger>();
+
+                logger.Error(ex, "Failed parsing text!");
+
+                return new HtmlString("");
+            }
         }
     }
 }
