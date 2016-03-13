25ead192f5b46c7215cd05ea66bb7344c913b0ba
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2016-03-13 15:02:28 +0100

    [FIX]

--- src/Jajo.Cms/Features/FeatureSettings.cs
+++ src/Jajo.Cms/Features/FeatureSettings.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Jajo.Cms.FubuMVC1.Features
+namespace Jajo.Cms.Features
 {
     public class FeatureSettings
     {
@@ -9,36 +9,36 @@ namespace Jajo.Cms.FubuMVC1.Features
         {
             Features = new List<Feature>();
         }
-
+    
         public IEnumerable<Feature> Features { get; set; }
-
+    
         public bool IsActive(string feature)
         {
             var parts = new Queue<string>(feature.Split('/'));
             Feature currentFeature = null;
-
+    
             while (parts.Any())
             {
                 var currentPart = parts.Dequeue();
-
+    
                 currentFeature = currentFeature == null
                     ? Features.FirstOrDefault(x => x.Name == currentPart)
                     : currentFeature.Children.FirstOrDefault(x => x.Name == currentPart);
-
+    
                 if (currentFeature == null || !currentFeature.Active)
                     return false;
             }
-
+    
             return true;
         }
-
+    
         public class Feature
         {
             public Feature()
             {
                 Children = new List<Feature>();
             }
-
+    
             public string Name { get; set; }
             public IEnumerable<Feature> Children { get; set; }
             public bool Active { get; set; }
