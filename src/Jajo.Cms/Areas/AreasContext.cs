879beb8acdae9d38e2feafe4bf19e4b36f781d80
MattiasJakobsson <mattias.jakobsson1988@gmail.com>
2017-05-18 14:51:19 +0200

    Added support for areas

---
+++ src/Jajo.Cms/Areas/AreasContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Jajo.Cms.Areas
+{
+    public class AreasContext
+    {
+        public static string Name = typeof(AreasContext).Name;
+
+        public static RequestContext Build(IReadOnlyDictionary<string, string> areas)
+        {
+            return new RequestContext(Name, new Dictionary<string, object>
+            {
+                {"Areas", areas}
+            });
+        }
+
+        public static string GetAreaFrom(RequestContext context, string area)
+        {
+            if (context?.Data == null || !context.Data.ContainsKey("Areas"))
+                return "";
+
+            var areas = context.Data["Areas"] as IReadOnlyDictionary<string, string>;
+
+            if (areas == null || !areas.ContainsKey(area))
+                return "";
+
+            return areas[area];
+        }
+    }
+}
---
+++ src/Jajo.Cms/Areas/AreasTextParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Jajo.Cms.Parsing;
+using Jajo.Cms.Rendering;
+using Jajo.Cms.Theme;
+
+namespace Jajo.Cms.Areas
+{
+    public class AreasTextParser : RegexTextParser
+    {
+        public override IEnumerable<string> GetTags()
+        {
+            yield return "areas";
+            yield return "advanced";
+            yield return "multitarget";
+        }
+
+        protected override object FindParameterValue(Match match, ICmsRenderer cmsRenderer, ICmsContext context, ITheme theme, Func<string, string> recurse)
+        {
+            var areaNameGroup = match.Groups["areaName"];
+
+            if (areaNameGroup == null)
+                return "";
+
+            var areaName = areaNameGroup.Value;
+
+            var areaContent = context
+                .FindContexts(AreasContext.Name)
+                .Select(x => AreasContext.GetAreaFrom(x, areaName))
+                .LastOrDefault(x => !string.IsNullOrEmpty(x));
+
+            return string.IsNullOrEmpty(areaContent) ? "" : recurse(areaContent);
+        }
+
+        protected override IEnumerable<Regex> GetRegexes()
+        {
+            yield return new Regex(@"\!\[Area\.((?<areaName>.[a-z&auml;&aring;&ouml;A-Z&Auml;&Aring;&Ouml;0-9*))\]\!", RegexOptions.Compiled);
+        }
+    }
+}
